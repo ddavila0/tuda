5bb6612e461c449ea2d80d4ce20640ae50ba79be
Jonathan Guiang <jkguiang@gmail.com>
2019-07-13 00:15:57 +0200

    tests are now more usefu

--- monit/aggs/tests.py
+++ monit/aggs/tests.py
@@ -1,5 +1,11 @@
 from .agg_utils import agg_wrapper
 
-@agg_wrapper(__file__, group_name="tests")
-def naive_test(df, chunked=False):
-    return
+@agg_wrapper(__file__, group_name="es_tests")
+def shape_test(df, chunked=False):
+    """Verify DataFrame is propery communicated to aggregators"""
+    return "you gave me a df this big: {}".format(df.shape)
+
+@agg_wrapper(__file__, group_name="es_tests")
+def column_test(df, chunked=False):
+    """Verify DataFrame is propery communicated to aggregators"""
+    return "columns: {}".format((", ").join(df.columns))

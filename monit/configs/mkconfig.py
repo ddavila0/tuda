d85ec3b5bd4b7aafd0667f7fe2f6cfe952edd635
Jonathan Guiang <jkguiang@gmail.com>
2019-07-16 00:14:15 +0200

    updated configs

--- monit/configs/mkconfig.py
+++ monit/configs/mkconfig.py
@@ -1,5 +1,10 @@
-{
+import json
+
+ex_config ={
     "source_name": "classads", 
     "hdfs_base": "/project/monitoring/archive/condor/raw/metric",
     "hdfs_ext": "json.gz"
 }
+ 
+with open("example.json", "w") as fout:
+    json.dump(ex_config, fout, indent=4, sort_keys=True)

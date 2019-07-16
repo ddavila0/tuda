a58753ddc99e74ebde3b116b1bc04ef831944fc3
Jonathan Guiang <jkguiang@gmail.com>
2019-07-16 21:37:04 +0200

    updated source_name

--- monit/aggs/tests.py
+++ monit/aggs/tests.py
@@ -1,9 +1,9 @@
 from .agg_utils import agg_wrapper
 
-@agg_wrapper(source_name="es_tests")
+@agg_wrapper(source_name="test")
 def shape_test(df):
     return "you gave me a df this big: {}".format(df.shape)
 
-@agg_wrapper(source_name="es_tests")
+@agg_wrapper(source_name="test")
 def column_test(df):
     return "columns: {}".format((", ").join(df.columns))

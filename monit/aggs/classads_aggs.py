4e033dc564db0d7d75b5668084e5e030492a8d07
Jonathan Guiang <jkguiang@gmail.com>
2019-07-20 01:03:06 +0200

    moved to pyspark aggregation functions

--- monit/aggs/classads_aggs.py
+++ monit/aggs/classads_aggs.py
@@ -1,37 +1,61 @@
+import pyspark.sql.functions as fn
+from pyspark.sql.types import StringType
 from .agg_utils import agg_wrapper
-import numpy as np
 
 # ClassAds aggregations
 @agg_wrapper(source_name="classads")
 def num_unique_jobs(df):
-    df["workflow_id"] = (df.workflow_id.map(str)
-                         + "_"
-                         + df.num_retries.astype(str))
-    df["job_id"] = (df.crab_id.map(str)
-                    + "/"
-                    + df.workflow_id)
-    return df.job_id.nunique()
+    agg = df.select(
+              fn.concat(
+                  df.crab_id,
+                  fn.lit("/"),
+                  df.workflow_id,
+                  fn.lit("_"),
+                  df.num_retries.cast(StringType())
+              ).alias("job_id"),
+          ).agg(fn.countDistinct("job_id").alias("num_unique_jobs"))
+
+    return agg.collect()[0]["num_unique_jobs"] 
 
 @agg_wrapper(source_name="classads")
 def num_unique_users(df):
-    return df.user_hn.nunique()
+    agg = df.agg(fn.countDistinct("user_hn").alias("num_unique_users"))
+    return agg.collect()[0]["num_unique_users"]
 
 @agg_wrapper(source_name="classads")
 def total_walltime(df):
-    return df.walltime.sum()
+    agg = df.agg(fn.sum("walltime").alias("total_walltime"))
+    return agg.collect()[0]["total_walltime"]
 
 @agg_wrapper(source_name="classads")
 def total_walltime_times_cpus(df):
-    return (df.walltime*df.num_cpus).sum()
+    agg = df.agg(fn.sum(df.walltime*df.num_cpus)
+                   .alias("total_walltime_times_cpus")
+                )
+    return agg.collect()[0]["total_walltime_times_cpus"]
 
 @agg_wrapper(source_name="classads")
 def total_cpu_time(df):
-    return df.cpu_time.sum()
+    agg = df.agg(fn.sum(df.cpu_time).alias("total_cpu_time"))
+    return agg.collect()[0]["total_cpu_time"]
 
 @agg_wrapper(source_name="classads")
-def exit_code_frac(df):
-    return np.sum(df.exit_code == 0)/df.shape[0]
+def num_exit_code_zero(df):
+    agg = df.agg(
+              fn.count(
+                  fn.when((df.exit_code == 0), True)
+              ).alias("num_exit_code_zero")
+          )
+    return agg.collect()[0]["num_exit_code_zero"]
+
+@agg_wrapper(source_name="classads")
+def num_exits(df):
+    return df.select("exit_code").count()
 
 @agg_wrapper(source_name="classads", post_agg=True)
 def cpu_eff(aggs):
     return aggs["total_cpu_time"]/aggs["total_walltime_times_cpus"]
+
+@agg_wrapper(source_name="classads", post_agg=True)
+def exit_code_frac(aggs):
+    return aggs["num_exit_code_zero"]/aggs["num_exits"]

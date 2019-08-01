2eec8547951dd507119400f59521c08873ff3930
Jonathan Guiang <jkguiang@gmail.com>
2019-08-02 01:29:23 +0200

    modified for use in container

--- monit/monicron.test.sh
+++ monit/monicron.test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_93/x86_64-centos7-gcc62-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/hadoop-swan-setconf.sh analytix
-spark-submit \
-    --conf spark.driver.port=5001 \
-    --conf spark.blockManager.port=5101 \
-    --conf spark.ui.port=5201 \
-    --conf spark.driver.extraClassPath='/eos/project/s/swan/public/hadoop-mapreduce-client-core-2.6.0-cdh5.7.6.jar' \
-    monicron.py $@ --outdir="/eos/user/j/jguiang/monitor/" --config="./configs/miniaod_classads.json"
+for config in ./configs/*.json; do
+    echo "Running aggregations for ${config}"
+    spark-submit \
+        --conf spark.driver.port=5001 \
+        --conf spark.blockManager.port=5101 \
+        --conf spark.ui.port=5201 \
+        --conf spark.driver.extraClassPath='/eos/project/s/swan/public/hadoop-mapreduce-client-core-2.6.0-cdh5.7.6.jar' \
+        monicron.py ${1} --outdir="${OUT_DIR}" --config="${config}"
+done

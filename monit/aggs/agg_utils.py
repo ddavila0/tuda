d451a1c88236d8463052de87828011eb4f091d27
Jonathan Guiang <jkguiang@gmail.com>
2019-08-01 23:06:42 +0200

    configured to handle multiple sources per agg

--- monit/aggs/agg_utils.py
+++ monit/aggs/agg_utils.py
@@ -8,9 +8,12 @@ class agg_wrapper():
        object responsible for the organization of MONIT's plugin system
     """
 
-    def __init__(self, source_name="aggs", post_agg=False):
-        self.source_name = source_name
+    def __init__(self, source_names=["aggs"], post_agg=False):
+        self.source_names = source_names
         self.agg_type = "post_aggs" if post_agg else "aggs"
+
+        if type(source_names) != list:
+            self.source_names = [str(source_names)]
 
     def __call__(self, func):
         self.update_config(func)
@@ -25,15 +28,16 @@ class agg_wrapper():
         """Update global config object"""
         global CONFIGS
         agg_type = self.agg_type
-        if self.source_name in CONFIGS:
-            config = CONFIGS[self.source_name]
-            if agg_type in config and func not in config[agg_type]:
-                config[agg_type].append(func)
-            elif agg_type not in config:
-                config[agg_type] = [func]
-        else:
-            config = {agg_type: [func]}
-            CONFIGS[self.source_name] = config
+        for source_name in self.source_names:
+            if source_name in CONFIGS:
+                config = CONFIGS[source_name]
+                if agg_type in config and func not in config[agg_type]:
+                    config[agg_type].append(func)
+                elif agg_type not in config:
+                    config[agg_type] = [func]
+            else:
+                config = {agg_type: [func]}
+                CONFIGS[source_name] = config
 
         return
 
@@ -53,8 +57,6 @@ def run_aggs(df, source_name="aggs"):
             for func in config["aggs"]:
                 results[func.__name__] = func(df)
 
-    print("Ran aggs for {}".format(source_name))
-    print("Results: {}".format(json.dumps(results, indent=4)))
     return results
 
 def run_post_aggs(results, source_name="aggs"):
@@ -73,14 +75,11 @@ def run_post_aggs(results, source_name="aggs"):
             for func in config["post_aggs"]:
                 results[func.__name__] = func(results)
 
-    print("Ran post-aggs for {}".format(source_name))
-    print("Results: {}".format(json.dumps(results, indent=4)))
     return results
 
 def add_aggs(current_aggs, aggs_to_add):
     """Add two aggregation results dictionaries together"""
     results = {a: current_aggs[a]+aggs_to_add[a] 
                for a in current_aggs.keys()}
-    print("Added aggs")
-    print("Results: {}".format(json.dumps(results, indent=4)))
+
     return results

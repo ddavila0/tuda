03e281902c03357761d8a9d600d052f7ac0d3e18
Jonathan Guiang <jkguiang@gmail.com>
2019-08-02 22:59:36 +0200

    now writes to test folder

--- monit/monicron.test.sh
+++ monit/monicron.test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 for config in ./configs/*.json; do
-    echo "Running aggregations for ${config}"
+    echo "[shell] Running aggregations for ${config}"
     spark-submit \
         --conf spark.driver.port=5001 \
         --conf spark.blockManager.port=5101 \
         --conf spark.ui.port=5201 \
         --conf spark.driver.extraClassPath='/eos/project/s/swan/public/hadoop-mapreduce-client-core-2.6.0-cdh5.7.6.jar' \
-        monicron.py ${1} --outdir="${OUT_DIR}" --config="${config}"
+        monicron.py ${1} --outdir="./test" --config="${config}"
 done

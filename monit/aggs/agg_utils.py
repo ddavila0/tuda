7cbcafa60eebaf8d3a0c4dcfaba5d8b31ecf58bb
Jonathan Guiang <jkguiang@gmail.com>
2019-08-12 23:28:33 +0200

    renamed source_name to tags, added ability to specify cache

--- monit/aggs/agg_utils.py
+++ monit/aggs/agg_utils.py
@@ -8,12 +8,12 @@ class agg_wrapper():
        object responsible for the organization of MONIT's plugin system
     """
 
-    def __init__(self, source_names=["aggs"], post_agg=False):
-        self.source_names = source_names
+    def __init__(self, tags=["aggs"], cache="", post_agg=False):
+        self.tags = tags if not cache else [cache+"_"+t for t in tags]
         self.agg_type = "post_aggs" if post_agg else "aggs"
 
-        if type(source_names) != list:
-            self.source_names = [str(source_names)]
+        if type(tags) != list:
+            self.tags = [str(tags)]
 
     def __call__(self, func):
         self.update_config(func)
@@ -28,46 +28,46 @@ class agg_wrapper():
         """Update global config object"""
         global CONFIGS
         agg_type = self.agg_type
-        for source_name in self.source_names:
-            if source_name in CONFIGS:
-                config = CONFIGS[source_name]
+        for tag in self.tags:
+            if tag in CONFIGS:
+                config = CONFIGS[tag]
                 if agg_type in config and func not in config[agg_type]:
                     config[agg_type].append(func)
                 elif agg_type not in config:
                     config[agg_type] = [func]
             else:
                 config = {agg_type: [func]}
-                CONFIGS[source_name] = config
+                CONFIGS[tag] = config
 
         return
 
-def run_aggs(df, source_name="aggs"):
+def run_aggs(df, tag="aggs"):
     """Run aggregations from a given source"""
     global CONFIGS
-    if not source_name in CONFIGS:
+    if not tag in CONFIGS:
         valid_sources = (", ").join(CONFIGS.keys())
         raise ValueError("invalid source name "
                          + "(current sources: {})".format(valid_sources))
     else:
         results = {}
-        config = CONFIGS[source_name]
+        config = CONFIGS[tag]
         if not "aggs" in config:
-            raise Exception("no aggregations in "+source_name)
+            raise Exception("no aggregations in "+tag)
         else:
             for func in config["aggs"]:
                 results[func.__name__] = func(df)
 
     return results
 
-def run_post_aggs(results, source_name="aggs"):
+def run_post_aggs(results, tag="aggs"):
     """Run post-aggregation aggregations from a given source"""
     global CONFIGS
-    if not source_name in CONFIGS:
+    if not tag in CONFIGS:
         valid_sources = (", ").join(CONFIGS.keys())
         raise ValueError("invalid source name "
                          + "(current sources: {})".format(valid_sources))
     else:
-        config = CONFIGS[source_name]
+        config = CONFIGS[tag]
         if not "post_aggs" in config:
             print("WARNING: no post-aggs to run")
             return results

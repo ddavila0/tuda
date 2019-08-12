b5f6da1c929812b9197b71f1c06d5cffcf3c4872
Jonathan Guiang <jkguiang@gmail.com>
2019-08-12 23:28:42 +0200

    renamed source_name to tags

--- monit/aggs/classads_aggs.py
+++ monit/aggs/classads_aggs.py
@@ -1,6 +1,6 @@
 from .agg_utils import agg_wrapper
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def num_unique_jobs(df):
     df["workflow_id"] = (df.workflow_id.map(str)
                          + "_"
@@ -10,38 +10,38 @@ def num_unique_jobs(df):
                     + df.workflow_id)
     return df.job_id.nunique()
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def num_unique_users(df):
     return df.user_hn.nunique()
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def total_walltime(df):
     return df.walltime.sum()
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def total_walltime_times_cpus(df):
     return (df.walltime*df.num_cpus).sum()
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def total_cpu_time(df):
     return df.cpu_time.sum()
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def num_exit_code_zero(df):
     return (df.exit_code == 0).sum()
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"])
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"])
 def num_exits(df):
     return df.shape[0]
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"], 
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"], 
              post_agg=True)
 def exit_code_frac(aggs):
     numer = float(aggs["num_exit_code_zero"])
     denom = float(aggs["num_exits"])
     return 0 if denom == 0 else numer
 
-@agg_wrapper(source_names=["classads/miniaod", "classads/nanoaod"],
+@agg_wrapper(tags=["classads_miniaod", "classads_nanoaod"],
              post_agg=True)
 def cpu_eff(aggs):
     numer = aggs["total_cpu_time"]
--- monit/aggs/xrootd_aggs.py
+++ monit/aggs/xrootd_aggs.py
@@ -1,42 +1,42 @@
 from .agg_utils import agg_wrapper
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"])
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"])
 def working_set(df, chunked=False):
     return (df[df.operation == "read"]
                 .drop_duplicates(["file_name", "file_size"])
                 .file_size.sum())/1e12
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"])
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"])
 def total_naive_reads(df):
     return df.file_size.sum()/1e12
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"])
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"])
 def total_actual_reads(df):
     return df.read_bytes.sum()/1e12
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"])
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"])
 def num_unique_file_accesses(df):
     return (df.groupby("file_name").app_info.nunique()).sum()
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"])
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"])
 def num_unique_files(df):
     return df.file_name.nunique()
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"], 
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"], 
              post_agg=True)
 def reuse_mult_1(aggs):
     numer = float(aggs["num_unique_file_accesses"])
     denom = float(aggs["num_unique_files"])
     return 0 if denom == 0 else numer/denom
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"], 
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"], 
              post_agg=True)
 def reuse_mult_2(aggs):
     numer = aggs["total_naive_reads"]
     denom = aggs["working_set"]
     return 0 if denom == 0 else numer/denom
 
-@agg_wrapper(source_names=["xrootd/mc", "xrootd/data", "xrootd/user"], 
+@agg_wrapper(tags=["xrootd_mc", "xrootd_data", "xrootd_user"], 
              post_agg=True)
 def reuse_mult_3(aggs):
     numer = aggs["total_actual_reads"]

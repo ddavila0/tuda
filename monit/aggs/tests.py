1be67b109b60f3bdf24c5563531cd83713c9ac7b
Jonathan Guiang <jkguiang@gmail.com>
2019-08-13 19:45:37 +0200

    removing unused tests

--- monit/aggs/tests.py
+++
@@ -1,9 +0,0 @@
-from .agg_utils import agg_wrapper
-
-@agg_wrapper(source_names="test")
-def shape_test(df):
-    return "you gave me a df this big: {}".format(df.shape)
-
-@agg_wrapper(source_names="test")
-def column_test(df):
-    return "columns: {}".format((", ").join(df.columns))

cceb316324cef732e7adc537c29bf929b06e3150
Jonathan Guiang <jkguiang@gmail.com>
2019-08-27 22:14:25 +0200

    fixed build args, added check for existing container

--- monit/run-docker.sh
+++ monit/run-docker.sh
@@ -9,14 +9,27 @@ input_loop() {
     fi 
 }
 
-echo "Building monicron image..."
-docker build --tag=monicron .
-echo "Collecting volume..."
-volume=`docker volume ls --quiet`
-if [[ -z ${volume} ]]; then
-    volume=`docker volume create monicron`
+existing=`docker ps --quiet`
+if [[ -z "${existing}" ]] ; then
+    if [[ -e "${1}" ]] ; then
+        echo "Building monicron image..."
+        docker build --tag=monicron --build-arg keytab="${1}" --build-arg user="${USER}" .
+        echo "Collecting volume..."
+        volume=`docker volume ls --quiet`
+        if [[ -z ${volume} ]]; then
+            volume=`docker volume create monicron`
+        fi
+        echo "Starting container..."
+        container=$(docker run -d -it -p 5001-5300:5001-5300 -v monicron:/aggregations --hostname tuda.cern.ch monicron:latest)
+        echo "Intialized container named ${container}"
+        input_loop ${container}
+    else
+        echo "Error: No keytab supplied or invalid path"
+        echo "Usage: ./run-docker </path/to/kt.keytab>"
+        exit 0
+    fi
+else
+    echo "Container already running!"
+    input_loop ${existing}
 fi
-echo "Starting container..."
-container=`docker run -d -it -p 5001-5300:5001-5300 -v monicron:/aggregations --hostname tuda.cern.ch monicron:latest`
-echo "Intialized container named ${container}"
-input_loop ${container}
+
